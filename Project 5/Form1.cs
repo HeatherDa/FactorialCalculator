6c8440947eb4deb50a7f8943add3700fe165284a
HeatherDa <crystal_tree@hotmail.com>
2017-02-18 21:32:24 -0600

    exception handling and data validation added

--- Project 5/Form1.cs
+++ Project 5/Form1.cs
@@ -19,18 +19,27 @@ namespace Project_5
 
         private void btnCalculate_Click(object sender, EventArgs e)
         {
-            //calculate the answer
-            int num = Convert.ToInt32(txtNumber.Text); //num is the integer the user entered
-            long fac = 1; //starting number
-                         
-            for(int i=num; i>0;i--){
-                fac = fac * i;//should be num* num-- etc until num=0
+            try
+            {
+                if (dataValidation())
+                {
+                    //calculate the answer
+                    int num = Convert.ToInt32(txtNumber.Text); //num is the integer the user entered
+                    long fac = 1; //starting number
 
+                    for (int i = num; i > 0; i--)
+                    {
+                        fac = fac * i;//should be num* num-- etc until num=0
+
+                    }
+
+                    string factorial = String.Format("{0:n0}", fac);//convert fac to string with thousand separators
+                    txtFactorial.Text = factorial;//display formatted string
+                    btnAC.Focus(); //prepare for the user to click All Clear
+                }
             }
-
-            string factorial = String.Format("{0:n0}", fac);//convert fac to string with thousand separators
-            txtFactorial.Text = factorial;//display formatted string
-            btnAC.Focus(); //prepare for the user to click All Clear
+            catch (FormatException) { MessageBox.Show("A format exception has occured.", "Format Exception"); }
+            catch (Exception) { MessageBox.Show("An exception has occured.", "Exception"); }
         }
 
         private void btnAC_Click(object sender, EventArgs e)
@@ -51,5 +60,46 @@ namespace Project_5
         {
         
         }
+        private bool dataValidation()
+        {
+            string n = "Number";
+            return IsPresent(txtNumber, n) && IsNumber(txtNumber, n) && IsInRange(txtNumber, n, 1, 20);
+        }
+        public bool IsPresent(TextBox textBox, string name) //is there data in the text box?
+        {
+            if (textBox.Text == "")
+            {
+                MessageBox.Show(name + " is a required field.", "Entry Error");
+                textBox.Focus();
+                return false;
+            }
+            return true;
+        }
+        public bool IsNumber(TextBox textBox, string name) //is the data numeric?
+        {
+            decimal numdec = 0m;
+            if (Decimal.TryParse(textBox.Text, out numdec))
+            {
+                return true;
+            }
+            else
+            {
+                MessageBox.Show(name + " must be a number.", "Entry Error");
+                textBox.Focus();
+                return false;
+            }
+        }
+        public bool IsInRange(TextBox textBox, string name, decimal min, decimal max) //is the number in the right range?
+        {
+            decimal number = Convert.ToDecimal(textBox.Text);
+            if (number < min || number > max)
+            {
+                MessageBox.Show(name + " must be between " + min.ToString() + " and " + max.ToString() + ".", "Entry Error");
+                textBox.Focus();
+                return false;
+            }
+            return true;
+        }
+
     }
 }
